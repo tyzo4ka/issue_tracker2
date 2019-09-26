2444fddcef6c3cffd9a12f0ec88a398a4338f997
tyzo4ka <tyzo4ka@gmail.com>
2019-09-26 13:08:10 +0600

    added base.html and index.html

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+# from webapp.forms import IssueForm
+from webapp.models import Issue
+from django.views.generic import View, TemplateView, RedirectView
 
-# Create your views here.
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['issues'] = Issue.objects.all()
+        return context
+
