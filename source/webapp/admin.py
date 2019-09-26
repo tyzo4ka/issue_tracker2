1d830399e18473ed083857a70393de3eba64b9be
tyzo4ka <tyzo4ka@gmail.com>
2019-09-26 12:18:12 +0600

    added admin.py

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from webapp.models import Issue, Status, Type
 
-# Register your models here.
+
+class IssueAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'summary', 'description', "status", "type", 'created_date']
+    list_filter = ['status', "type"]
+    list_display_links = ['pk', 'summary']
+    search_fields = ['summary', 'description']
+    exclude = []
+    readonly_fields = ['created_date']
+
+
+admin.site.register(Issue, IssueAdmin)
+admin.site.register(Status)
+admin.site.register(Type)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -6,7 +6,7 @@ class Issue(models.Model):
     description = models.TextField(max_length=5000, null=True, blank=True, verbose_name="Full description")
     status = models.ForeignKey("Status", on_delete=models.PROTECT, null=False, blank=False, verbose_name="Status")
     type = models.ForeignKey("Type", on_delete=models.PROTECT, null=False, blank=False, verbose_name="Type")
-    create_date = models.DateTimeField(auto_now_add=True, verbose_name="Date created")
+    created_date = models.DateTimeField(auto_now_add=True, verbose_name="Date created")
 
     def __str__(self):
         return self.summury
