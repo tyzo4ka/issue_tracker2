4db6cc0d6ad8971d20396f2e1520f194eab36b64
tyzo4ka <tyzo4ka@gmail.com>
2019-09-26 18:50:53 +0600

    fix bug in forms.py for status check and type check

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -6,8 +6,10 @@ from webapp.models import Status, Type
 class IssueForm(forms.Form):
     summary = forms.CharField(max_length=300, required=True, label="Summary")
     description = forms.CharField(max_length=5000, required=True, label="Description", widget=widgets.Textarea)
-    status = forms.ModelChoiceField(queryset=Status.objects.all(), required=True, label="Status", empty_label=None)
-    type = forms.ModelChoiceField(queryset=Type.objects.all(), required=True, label="Type", empty_label=None)
+    status = forms.ModelChoiceField(to_field_name='state', queryset=Status.objects.all(), required=True, label="Status",
+                                    empty_label=None)
+    type = forms.ModelChoiceField(to_field_name="name", queryset=Type.objects.all(), required=True, label="Type",
+                                  empty_label=None)
 
 
 class StatusForm(forms.Form):
