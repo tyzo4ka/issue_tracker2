a87b5395d0e93f54a70d32adc4a3c4c4e5cc5932
tyzo4ka <tyzo4ka@gmail.com>
2019-10-14 14:44:32 +0600

    Added search form in projects

--- source/webapp/views/project_views.py
+++ source/webapp/views/project_views.py
@@ -1,6 +1,8 @@
 from django.urls import reverse, reverse_lazy
-from webapp.forms import ProjectForm, ProjectIssueForm
+from webapp.forms import ProjectForm, ProjectIssueForm, SimpleSearchForm
 from webapp.models import Project
+from django.db.models import Q
+from django.utils.http import urlencode
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.core.paginator import Paginator
 from django.http import HttpResponseRedirect
@@ -13,6 +15,33 @@ class ProjectView(ListView):
     ordering = ['created_date']
     paginate_by = 5
     paginate_orphans = 1
+
+    def get(self, request, *args, **kwargs):
+        self.form = self.get_search_form()
+        self.search_query = self.get_search_query()
+        return super().get(request, *args, **kwargs)
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(object_list=object_list, **kwargs)
+        if self.search_query:
+            context['query'] = urlencode({'search': self.search_query})
+        context['form'] = self.form
+        return context
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if self.search_query:
+            queryset = queryset.filter(
+                Q(name__icontains=self.search_query) )
+        return queryset
+
+    def get_search_form(self):
+        return SimpleSearchForm(self.request.GET)
+
+    def get_search_query(self):
+        if self.form.is_valid():
+            return self.form.cleaned_data['search']
+        return None
 
 
 class ProjectDetailView(DetailView):
