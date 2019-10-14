6c0e15311d74cbe65fc79a5eca8bdcc3afa4e4ca
tyzo4ka <tyzo4ka@gmail.com>
2019-10-14 19:17:50 +0600

    changed queries

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -5,7 +5,7 @@ class Issue(models.Model):
     summary = models.CharField(max_length=300, null=False, blank=False, verbose_name="Small description")
     description = models.TextField(max_length=5000, null=True, blank=True, verbose_name="Full description")
     status = models.ForeignKey("Status", on_delete=models.PROTECT, null=False, blank=False, verbose_name="Status")
-    type = models.ForeignKey("Type", on_delete=models.PROTECT, null=False, blank=False, verbose_name="Type")
+    type = models.ForeignKey("Type", on_delete=models.PROTECT, null=False, blank=False, verbose_name="Type", related_name="issue_type")
     project = models.ForeignKey("Project", on_delete=models.PROTECT, null=True, blank=False, related_name="issues",
                                 verbose_name="Project")
     created_date = models.DateTimeField(auto_now_add=True, verbose_name="Date created")
