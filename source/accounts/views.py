e2cc46a101370672f04c9c3b48e2f84f5a2e3dca
tyzo4ka <tyzo4ka@gmail.com>
2019-10-24 19:06:08 +0600

    fixed bug with index page

--- source/accounts/views.py
+++ source/accounts/views.py
@@ -12,10 +12,7 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            if redirect_page is not None:
-                return redirect(redirect_page)
-            else:
-                return redirect('webapp:index')
+            return redirect(redirect_page)
         else:
             context['has_error'] = True
     return render(request, 'login.html', context=context)
