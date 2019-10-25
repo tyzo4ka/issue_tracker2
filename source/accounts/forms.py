a235fdac0712d421a3d7bd5b5b47e9e64e5c05fe
tyzo4ka <tyzo4ka@gmail.com>
2019-10-25 14:31:32 +0600

    Added UserCreationForm, added validation for fields username, password and password_confirm, email, first_name or last_name

---
+++ source/accounts/forms.py
@@ -0,0 +1,42 @@
+from django.contrib.auth.models import User
+from django import forms
+from django.core.exceptions import ValidationError
+
+
+class UserCreationForm(forms.ModelForm):
+    username = forms.CharField(max_length=100, label="Username", required=True)
+    password = forms.CharField(max_length=100, label="Password", required=True, widget=forms.PasswordInput)
+    password_confirm = forms.CharField(max_length=100, label="Confirm Password", required=True, widget=forms.PasswordInput)
+    email = forms.EmailField(label='Email', required=True)
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        try:
+            User.objects.get(username=username)
+            raise ValidationError("User with this username already exists", code="username_exists")
+
+    def clean(self):
+        super().clean()
+        password_1 = self.cleaned_data['password']
+        password_2 = self.cleaned_data["password_confirm"]
+        data = self.cleaned_data
+        if not data.get('first_name') and not data.get('last_name'):
+            raise ValidationError("One of the following fields: first_name, last_name should be filled",
+                                  code='first_name_last_name_criteria_empty')
+        if password_1 != password_2:
+            raise ValidationError("Passwords does not match", code="passwords_does_not_match")
+        return self.cleaned_data
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        try:
+            User.objects.get(email=email)
+            raise ValidationError('User with this email already exists', code='user_email_exists')
+        except User.DoesNotExist:
+            return email
+
+
+    class Meta:
+        model = User
+        fields = ['username', 'password', "password_confirm", 'first_name', 'last_name', 'email']
+
