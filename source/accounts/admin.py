35898a490244f1baffb0f755328d15824c4f8887
tyzo4ka <tyzo4ka@gmail.com>
2019-11-04 15:52:01 +0600

    register Team model in admin.py

--- source/accounts/admin.py
+++ source/accounts/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
-from .models import Profile
+from .models import Profile, Team
 
 
 class ProfileInline(admin.StackedInline):
@@ -15,3 +15,4 @@ class ProfileAdmin(UserAdmin):
 
 admin.site.unregister(User)
 admin.site.register(User, ProfileAdmin)
+admin.site.register(Team)
