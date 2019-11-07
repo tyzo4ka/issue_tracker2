c538d1ecf9aed4563edef895c5ec3d48e752986c
tyzo4ka <tyzo4ka@gmail.com>
2019-11-07 15:19:45 +0600

    Created new field in Issue model - assigned_to

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -10,13 +10,16 @@ class Issue(models.Model):
     summary = models.CharField(max_length=300, null=False, blank=False, verbose_name="Small description")
     description = models.TextField(max_length=5000, null=True, blank=True, verbose_name="Full description")
     status = models.ForeignKey("Status", on_delete=models.PROTECT, null=False, blank=False, verbose_name="Status")
-    type = models.ForeignKey("Type", on_delete=models.PROTECT, null=False, blank=False, verbose_name="Type", related_name="issue_type")
+    type = models.ForeignKey("Type", on_delete=models.PROTECT, null=False, blank=False, verbose_name="Type",
+                             related_name="issue_type")
     project = models.ForeignKey("Project", on_delete=models.PROTECT, null=True, blank=False, related_name="issues",
                                 verbose_name="Project")
     created_date = models.DateTimeField(auto_now_add=True, verbose_name="Date created")
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Time updated')
     created_by = models.ForeignKey(User, on_delete=models.PROTECT, null=False, blank=False, default=get_admin,
-                                   related_name="issues", verbose_name="Created by")
+                                   related_name="issues_created", verbose_name="Created by")
+    assigned_to = models.ForeignKey(User, on_delete=models.PROTECT, null=False, blank=False, default=get_admin,
+                                    related_name="issues_assigned", verbose_name="Assigned to")
 
     def __str__(self):
         return self.summary
